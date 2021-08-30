4171307efb0a335abf3014b7ca11132d49d1ca26
SPZCNSK <sapiezcinski.juan@gmail.com>
2021-08-30 09:41:42 -0300

    alteracao erro 500 para erro 401 Unauthorized em entrar.component.ts

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -42,7 +42,7 @@ export class EntrarComponent implements OnInit {
 
       this.router.navigate(['/inicio'])
     }, erro => {
-      if(erro.status == 500){
+      if(erro.status == 401){
         alert('Usuário ou senha estão incorretos')
       }
     })
