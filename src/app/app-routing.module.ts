221074a96694d23c96b4e544d7c6aad9b1776a28
SPZCNSK <sapiezcinski.juan@gmail.com>
2021-09-02 15:37:05 -0300

    componentes atualizar-put e deletar temas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 
 // Array de objetos de rotas
 const routes: Routes = [
@@ -18,7 +20,11 @@ const routes: Routes = [
 
   {path: 'inicio', component: InicioComponent},
 
-  {path: 'tema', component: TemaComponent}
+  {path: 'tema', component: TemaComponent},
+
+  {path: 'tema-edit/:id', component: TemaEditComponent},
+
+  {path: 'tema-delete/:id', component: TemaDeleteComponent}
 ];
 
 @NgModule({
--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -20,8 +20,21 @@ export class TemaService {
     return this.http.get<Tema[]>("https://blogpessjuan.herokuapp.com/tema", this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://blogpessjuan.herokuapp.com/tema/${id}`, this.token)
+  }
+
   // sempre que se cria um post é preciso passar uma variável e uma model
   postTema(temaCadastro: Tema): Observable<Tema>{
     return this.http.post<Tema>("https://blogpessjuan.herokuapp.com/tema", temaCadastro, this.token)
   }
+
+  putTema(temaCadastro: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogpessjuan.herokuapp.com/tema', temaCadastro, this.token)
+  }
+
+  deleteTema(id: number){
+    return this.http.delete(`https://blogpessjuan.herokuapp.com/tema/${id}`, this.token) //template literals -> passa uma rota com string e variavel simultaneamente
+  }
+
 }
